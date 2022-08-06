238f0898ebb1ffbb77ac54c589c731074fdbc359
jyothikiran843 <jyothikiranthammana843@gmail.com>
2022-08-06 12:29:18 +0530

    new

--- practice/settings.py
+++ practice/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import django_on_heroku
+django_on_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
